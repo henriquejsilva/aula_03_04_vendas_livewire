ecac47c25c11da82637d8047d8ee31d5638bdbb2
henriquejsilva <henriquejosemartinsdasilva@gmail.com>
2025-04-03 13:59:41 -0300

    first commit

---
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>Administrador Logado</h1>
+    {{-- <h4> {{ auth()-> user()->email}} </h4>
+    <h4> {{ auth()-> user()->id}} </h4>
+    <h4> {{ auth()-> user()->user_type}} </h4>
+    <h4> {{ auth()-> user()->admin->cpf}} </h4>
+    <h4> {{ auth()-> user()->admin->nome}} </h4>
+    <h4> {{ auth()-> user()->admin->created_at}} </h4>
+    <h4> {{ auth()-> user()->admin->id}} </h4> --}}
+</div>
